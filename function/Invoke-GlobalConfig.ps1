4ac5bb17cd84eaf17124a51042816e6dcea9abc2
captain <captain@sgc.local>
2018-04-08 23:54:24 -0700

    adding aerror handling to invoke global

--- function/Invoke-GlobalConfig.ps1
+++ function/Invoke-GlobalConfig.ps1
@@ -9,13 +9,18 @@ function Invoke-GlobalConfig() {
             $ModuleRoot = $($(Get-Item -Path $(Get-Module -Name 'sqlshell').Path).Directory).FullName
 
             $ConfigPath = Join-Path -Path $ModuleRoot -ChildPath '\config\sqlshellconfig.json'
+        } elseif (!$(Test-path $ConfigPath)) { 
+            $error = "The Config Path: {0} is not valid" -f $ConfigPath
+            Write-Host $error 
+            continue 
+            
         }
     }
 
     process { 
         [pscustomobject]$ConfigObject = Get-Content -Raw -Path $ConfigPath | ConvertFrom-Json 
 
-        $global:sqlshell = $ConfigObject
+        $global:sqlshell = $ConfigObjectf
 
     }
 }
